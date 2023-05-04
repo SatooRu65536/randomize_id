4ad8fa23f38b7ca8da3b6f9c08cceab7b3a103d8
SatooRu65536 <ryuto4619@gmail.com>
2023-05-04 13:50:43 +0900

    更新

--- script.js
+++ script.js
@@ -1,26 +1,81 @@
 let stunumData = [{ stunum: "", isSelected: false }];
+let selectedStunum = [];
 
 window.onload = () => {
+  initStunumData();
+
   document.addEventListener("scroll", () => {
-    if (Math.ceil(window.scrollY) >= window.innerHeight) {
-      window.scrollTo(0, 0);
-      document.getElementsByClassName("view")[0].scrollLeft = 0;
-    }
+    if (Math.ceil(window.scrollY) >= window.innerHeight) reset();
   });
 
-  initStunumData();
+  document
+    .getElementsByClassName("container")[0]
+    .addEventListener("click", selectNext);
 };
 
+// ローカルストレージからstunumDataを取得し更新する
 function initStunumData() {
   const localStunumData = JSON.parse(localStorage.getItem("stunumData"));
   stunumData = localStunumData ? localStunumData : stunumData;
 
   const box = document.getElementsByClassName("box")[0];
+
   stunumData.map((data) => {
     appendStuDataHTML(box, data.stunum, data.isSelected);
+    if (data.isSelected) selectedStunum.push(data.stunum);
   });
 }
 
+// リセットする
+function reset() {
+  document.getElementsByClassName("stu_num")[0].innerText = "-";
+  document.getElementsByClassName("view")[0].scrollLeft = 0;
+
+  selectedStunum = [];
+  stunumData = stunumData.map((data) => {
+    return { ...data, isSelected: false };
+  });
+
+  localStorage.setItem("stunumData", JSON.stringify(stunumData));
+
+  const cbx = document.getElementsByClassName("cbx");
+  for (let i = 0; i < cbx.length; i++) cbx[i].checked = false;
+
+  window.scrollTo(0, 0);
+}
+
+// 次のstunumを選択する
+function selectNext() {
+  // stuDataのうちselectedstunumに含まれないものをランダムで選択
+  const unselectedStunum = stunumData.filter(
+    (data) => !selectedStunum.includes(data.stunum) && data.stunum !== ""
+  );
+
+  if (unselectedStunum.length === 0) {
+    document.getElementsByClassName("stu_num")[0].innerText = "fin";
+    return;
+  }
+
+  const randomIndex = Math.floor(Math.random() * unselectedStunum.length);
+  const nextStunum = unselectedStunum[randomIndex];
+
+  // 選択したstunumをselectedstunumに追加
+  const index = stunumData.indexOf(nextStunum);
+  selectedStunum.push(nextStunum.stunum);
+  stunumData[index].isSelected = true;
+
+  // 選択したstunumをローカルストレージに保存
+  localStorage.setItem("stunumData", JSON.stringify(stunumData));
+
+  // 選択したstunumを表示
+  document.getElementsByClassName("stu_num")[0].innerText = nextStunum.stunum;
+
+  // チェックボックスをチェック
+  const cbx = document.getElementsByClassName("cbx")[index];
+  cbx.checked = true;
+}
+
+// stunumを変更したときの処理
 function changeStunum(e) {
   if (e.type === "keydown" && e.key !== "Enter") return;
 
@@ -28,7 +83,10 @@ function changeStunum(e) {
     e.target.parentNode
   );
   stunumData[index].stunum = e.target.value;
-  stunumData = stunumData.filter((data) => data.stunum != "");
+  const stunums = stunumData.map((data) => data.stunum);
+  stunumData = stunumData.filter((data, i) => {
+    return data.stunum != "" && stunums.lastIndexOf(data.stunum) === i;
+  });
   stunumData.unshift({ stunum: "", isSelected: false });
 
   localStorage.setItem("stunumData", JSON.stringify(stunumData));
@@ -49,14 +107,22 @@ function changeStunum(e) {
   }
 }
 
+// チェックボックスをクリックしたときの処理
 function clickCbx(e) {
   const index = Array.from(e.target.parentNode.parentNode.children).indexOf(
     e.target.parentNode
   );
   stunumData[index].isSelected = e.target.checked;
   localStorage.setItem("stunumData", JSON.stringify(stunumData));
+
+  if (e.target.checked) {
+    selectedStunum.push(stunumData[index].stunum);
+  } else {
+    selectedStunum.splice(selectedStunum.indexOf(stunumData[index].stunum), 1);
+  }
 }
 
+// inputたちを追加する
 function appendStuDataHTML(parent, stunum, isSelected) {
   const div = document.createElement("div");
 
